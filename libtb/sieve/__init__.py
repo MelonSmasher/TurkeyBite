dee2b4f8cc25258f6e23af36d9bc5d304e7bc77d
Alex Markessinis <markea125@gmail.com>
2019-11-07 14:40:28 -0500

    fixing dropped browser history

--- libtb/sieve/__init__.py
+++ libtb/sieve/__init__.py
@@ -70,9 +70,8 @@ class Filters(object):
         # Dive down into the data structure
         if 'data' in data.keys():
             if 'event' in data['data'].keys():
-                print(data)
                 if 'data' in data['data']['event'].keys():
-
+                    print(data)
                     # Client level rules
                     if 'client' in data['data']['event']['data'].keys():
 
